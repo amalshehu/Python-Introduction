5d695294ce0058bf056785ff2d6a643cd67cb77d
Amal Shehu <amalshehu@gmail.com>
2016-08-25 19:19:16 +0530

    total trip cost function updated

--- taking_a_vacation_codecademy.py
+++ taking_a_vacation_codecademy.py
@@ -1,1 +1,31 @@
-#taking a tour 
+def hotel_cost(nights):
+    return 140*nights
+
+
+def plane_ride_cost(city):
+
+        if (city=="Charlotte"):
+        return 183
+
+    elif city=="Tampa":
+        return 220
+
+    elif city=="Pittsburgh":
+        return 222
+
+    elif city=="Los Angeles":
+        return 475
+        
+
+def rental_car_cost(days):
+    rent= 40 * days
+
+    if days >= 7:
+        rent -= 50
+
+    elif days >= 3:
+        rent -= 20
+    return rent
+
+    def trip_cost(city,days):
+        return rental_car_cost(days)+plane_ride_cost(city)+hotel_cost(days)
