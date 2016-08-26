a35602d8f0e80b00bbdcf998069797ddeb71e787
Amal Shehu <amalshehu@gmail.com>
2016-08-26 23:49:00 +0530

    Added comments

--- tuple.py
+++ tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-tup1 = (12, 34.56);
-tup2 = ('abc', 'xyz');
+tup1 = (12, 34.56); #Tuple 1
+tup2 = ('abc', 'xyz');#Tuple 2
 tup3 = tup1 + tup2;
-print tup3
+print tup3 #Program output
