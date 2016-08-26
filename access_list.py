2d5a6d64d2646d8cae207413039057bfdb42c80a
Amal Shehu <amalshehu@gmail.com>
2016-08-26 13:41:18 +0530

    print list values

--- access_list.py
+++ access_list.py
@@ -2,3 +2,8 @@
 
 list1 = ['Data Structures', 'Operating System', 2014, 2015]
 list2 = [1, 2, 3, 4, 5, 6, 7]
+
+print ("list1[0]: ", list1[0])
+print ("list2[1:5]: ", list2[1:5])
+print ("list1[1]", list1[1])
+print ("list2[5:7] ", list2[5:7])
