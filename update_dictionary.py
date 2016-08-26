d5be42e18ea54460005481fbeb1f6949a76c2179
Amal Shehu <amalshehu@gmail.com>
2016-08-26 18:43:53 +0530

    Input data feature added

--- update_dictionary.py
+++ update_dictionary.py
@@ -2,9 +2,12 @@
 
 person = {'Name': 'Amal Shehu', 'Age': 23, 'Blood Group': 'A+'}
 
-person['Age'] = 24  # update existing entry
+person['Name'] = input("Enter your name :")
+
+person['Age'] = input("Enter your age :")  # update existing entry
+person['Blood Group'] = input("Enter your Blood Group :")
 person['Language'] = "Python"  # Add new entry
 
-
+print ("Name :", person['Name'])
 print ("person['Age']: ", person['Age'])
 print ("person['Language']: ", person['Language'])
