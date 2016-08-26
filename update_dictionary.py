be383bde5074504dd1bcf86ea47cd06e27c59049
Amal Shehu <amalshehu@gmail.com>
2016-08-26 18:59:40 +0530

    Completed-Print data with condition

--- update_dictionary.py
+++ update_dictionary.py
@@ -8,9 +8,18 @@ person['Age'] = input("Enter your age :")  # update existing entry
 person['Blood Group'] = input("Enter your Blood Group :")
 person['Language'] = "Python"
 
+c = input("Do you want to print the details ? (y/n)")
 
-print ("############################################################")
-print ("Name :", person['Name'])
-print ("Age: ", person['Age'])
-print ("Blood Group :", person['Blood Group'])
-print ("person['Language']: ", person['Language'])
+if (c == 'y'):
+    print("############################################################")
+    print ("Name :", person['Name'])
+    print ("Age: ", person['Age'])
+    print ("Blood Group :", person['Blood Group'])
+    print ("person['Language']: ", person['Language'])
+
+elif (c == 'n'):
+    print ("Thank You! Have a nice day!!!")
+
+
+else:
+    print ("Invalid Input")
