8091cf99884813ff2778b88a5fcc221fe1c8f29c
Amal Shehu <amalshehu@gmail.com>
2016-08-26 13:55:40 +0530

    Update list value

--- access_list.py
+++ access_list.py
@@ -7,3 +7,7 @@ print ("list1[0]: ", list1[0])
 print ("list2[1:5]: ", list2[1:5])
 print ("list1[1]", list1[1])
 print ("list2[5:7] ", list2[5:7])
+
+print ("Value  at index 2 : ", list1[2])
+list1[2] = 2016
+print ("New value at index 2 : ", list1[2])
