7a0bf216ce501b868ad958d6c0ac56bb4b71c35e
Amal Shehu <amalshehu@gmail.com>
2016-08-26 18:29:35 +0530

    update values- Dictionary in python

---
+++ update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+person = {'Name': 'Amal Shehu', 'Age': 23, 'Blood Group': 'A+'}
+
+person['Age'] = 24  # update existing entry
+person['Language'] = "Python"  # Add new entry
+
+
+print ("person['Age']: ", person['Age'])
+print ("person['Language']: ", person['Language'])
