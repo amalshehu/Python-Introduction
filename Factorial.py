4c6af5ecc5cae2632319403eedfea23b227c0696
Amal Shehu <amalshehu@gmail.com>
2016-08-27 22:17:34 +0530

    convert indent to space / reformat files based on PEP8

--- Factorial.py
+++ Factorial.py
@@ -1,4 +1,4 @@
- #Python program to find the factorial of a number
+# Python program to find the factorial of a number
 
 
 num = int(input("Enter a number: "))
@@ -6,10 +6,10 @@ factorial = 1
 
 
 if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
+    print("factorial does not exist for negative numbers")
 elif num == 0:
-   print("The factorial of 0 is 1")
+    print("The factorial of 0 is 1")
 else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+    for i in range(1, num + 1):
+        factorial = factorial*i
+    print("The factorial of", num, "is", factorial)
