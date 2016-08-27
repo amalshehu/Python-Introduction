49fbdc3e9cd031221a6bce364a75f12c73604015
Amal Shehu <amalshehu@gmail.com>
2016-08-27 22:10:47 +0530

    condition added to check if the number is negative, positive or zero

--- Factorial.py
+++ Factorial.py
@@ -1,8 +1,15 @@
-# program to find the factorial of a number
+ #Python program to find the factorial of a number
 
-num = (input("Enter a number: "))
+
+num = int(input("Enter a number: "))
 factorial = 1
 
- for i in range(1,num + 1):
-    factorial = factorial*i
-    print("The factorial of",num,"is",factorial)
+
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
