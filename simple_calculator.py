924fc670925c18a9b0a564cbcafbbe0194c7e656
Amal Shehu <amalshehu@gmail.com>
2016-08-27 23:11:46 +0530

    convert indent to space / reformat files based on PEP8

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,15 +1,19 @@
 # simple calculator
 
-def add(x,y):
+
+def add(x, y):
     return x+y
 
-def mult(x,y):
+
+def mult(x, y):
     return x*y
 
-def sub(x,y):
+
+def sub(x, y):
     return x-y
 
-def div(x,y):
+
+def div(x, y):
     return x/y
 
 print("1.Add")
@@ -21,15 +25,15 @@ ch = input("Enter your ch :")
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 if ch == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
+    print(num1, "+", num2, "=", add(num1, num2))
 
 elif ch == '2':
-   print(num1,"-",num2,"=", sub(num1,num2))
+    print(num1, "-", num2, "=", sub(num1, num2))
 
 elif ch == '3':
-   print(num1,"*",num2,"=", mult(num1,num2))
+    print(num1, "*", num2, "=", mult(num1, num2))
 
 elif ch == '4':
-   print(num1,"/",num2,"=", div(num1,num2))
+    print(num1, "/", num2, "=", div(num1, num2))
 else:
-   print("Invalid input")
+    print("Invalid input")
