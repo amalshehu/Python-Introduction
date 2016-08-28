d1c99c760fb3e12efdfcb8ccf99dde47fd4a9a38
Amal Shehu <amalshehu@gmail.com>
2016-08-28 23:47:02 +0530

    Comments added

--- factorial_recursion.py
+++ factorial_recursion.py
@@ -4,14 +4,14 @@
 #  Course:     Practice
 #  Date:       Sunday 28th August 2016, 11:10 PM
 
-num = int(input("Enter a number"))
+num = int(input("Enter a number"))  # Convert to an int
 
 
 def factorial(num):
     if (num == 0):
         return
     else:
-        return num*factorial(num-1)
+        return num*factorial(num-1)  # Factorial using recursion
 
 result = factorial(num)
 print(result)
