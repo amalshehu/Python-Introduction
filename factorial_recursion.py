b5870231ba93fa9949582a89e8707ddf80052c3e
Amal Shehu <amalshehu@gmail.com>
2016-08-28 23:45:15 +0530

    convert indent to space / reformat files based on PEP8

--- factorial_recursion.py
+++ factorial_recursion.py
@@ -4,14 +4,14 @@
 #  Course:     Practice
 #  Date:       Sunday 28th August 2016, 11:10 PM
 
+num = int(input("Enter a number"))
 
 
-num=  int(input ("Enter a number"))
 def factorial(num):
-    if (num==0):
-        return 1
+    if (num == 0):
+        return
     else:
         return num*factorial(num-1)
 
-result=factorial(num)
+result = factorial(num)
 print(result)
