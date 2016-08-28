5a929b4663fde5cd3d29c941eb8c527ecc6db11f
Amal Shehu <amalshehu@gmail.com>
2016-08-28 23:54:11 +0530

    Escape sequence test

---
+++ escape_sequence.py
@@ -0,0 +1,10 @@
+#  File:       escape_sequence.py
+#  Purpose:    Example: an escape sequence counts as one character
+#  Programmer: Amal Shehu
+#  Course:     Practice
+#  Date:       Sunday 28th August 2016, 11:50 PM
+
+
+s = 'one\ntwo\tthree'
+print (s)
+print (len(s))
