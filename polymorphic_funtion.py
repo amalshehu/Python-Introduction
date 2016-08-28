793b1291323231f792d57fb0b02038c6a0ffaa1b
Amal Shehu <amalshehu@gmail.com>
2016-08-28 20:34:36 +0530

    polymorphic_function

---
+++ polymorphic_funtion.py
@@ -0,0 +1,13 @@
+#  File:       polymorphic_function.py
+#  Purpose:    Example: polymorphic_function
+#  Programmer: Amal Shehu
+#  Course:     Practice
+#  Date:       Sunday 28th August 2016, 08:28 PM
+
+def twice(x):
+    return (2 * x)
+
+y = 5
+print (twice(y))
+z = "Spam "
+print (twice(z))
