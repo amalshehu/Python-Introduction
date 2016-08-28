2af6b77ba874e870ac05617e877de63427011a44
Amal Shehu <amalshehu@gmail.com>
2016-08-28 20:20:03 +0530

    Loop inside loop completed successfully

--- loop_inside_loop.py
+++ loop_inside_loop.py
@@ -1,5 +1,5 @@
 #  File:       loop_inside_loop.py
-#  Purpose:    Example: how to use a loop inside a loop,
+#  Purpose:    Example: how to use a loop inside a loop
 #              a nested while loop
 
 #  Programmer: Amal Shehu
@@ -10,3 +10,23 @@
 print ("Program Started")
 
 x = 1
+while (x < 6):
+    print() # prints a new line
+    print ("x = " + str(x)) #  printing of the next item
+                            # to be on the same line
+    x = x + 1
+    y = 1
+    while (y < 6):
+        print ("y = " + str(y),) #  printing on the same line
+        y = y + 1
+        z = 1
+        while (z < 6):
+            print ("z = " + str(z),) #  printing on the same line
+            z = z + 1
+        print() # prints a new line
+'''
+See that with a loop repeating 5 times,
+inside a loop that repeats 5 times
+inside a loop that repeats 5 times
+means that we can control 125 processes.
+'''
