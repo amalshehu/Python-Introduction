3364fa34b42ec23fb72e8469125d867076fc3648
Amal Shehu <amalshehu@gmail.com>
2016-08-28 21:46:59 +0530

    Demonstration of functions

---
+++ use_of_funtions.py
@@ -0,0 +1,18 @@
+#  File:       use_of_function.py
+#  Purpose:    Example: use of a function
+#  Programmer: Amal Shehu
+#  Course:     Practice
+#  Date:       Sunday 28th August 2016, 08:55 PM
+
+'''
+
+
+    This is a muliti-line comment (')
+
+    Functions can be defined anywhere.But note that
+    to define before calling it.
+
+    We are using a  simple pause function,
+    to pause a program until a key is pressed.
+
+'''
