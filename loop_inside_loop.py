bf58327f16771bf093d24fc389061d5d1e3ddd93
Amal Shehu <amalshehu@gmail.com>
2016-08-28 20:09:43 +0530

    Loop inside loop

---
+++ loop_inside_loop.py
@@ -0,0 +1,12 @@
+#  File:       loop_inside_loop.py
+#  Purpose:    Example: how to use a loop inside a loop,
+#              a nested while loop
+
+#  Programmer: Amal Shehu
+#  Course:     Practice
+#  Date:       Sunday 28th August 2016, 08:05 PM
+
+
+print ("Program Started")
+
+x = 1
