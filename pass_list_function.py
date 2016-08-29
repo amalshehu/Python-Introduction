b3b5de4a2e60839c9d975d3c7f8b970f893aee2e
Amal Shehu <amalshehu@gmail.com>
2016-08-29 16:33:40 +0530

    Passing a list to a function

---
+++ pass_list_function.py
@@ -0,0 +1,12 @@
+#  File:       pass_list_function.py
+#  Purpose:    Example : Passing a list to a function
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Monday 29th August 2016, 04:30 PM
+
+
+def list_function(x):
+    return x
+
+n = [3, 5, 7]
+print list_function(n)
