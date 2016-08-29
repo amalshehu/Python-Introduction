ae70072e3f1d2df95acc797470dba9f72f491373
Amal Shehu <amalshehu@gmail.com>
2016-08-29 12:05:11 +0530

    convert indent to space / reformat files based on PEP8

--- supermarket.py
+++ supermarket.py
@@ -1,20 +1,20 @@
 #  File:       supermarket.py
-#  Purpose:    Example: Python dictionaty
+#  Purpose:    Example: Python dictionary
 #  Programmer: Amal Shehu
 #  Course:     Codecademy
 #  Date:       Monday 29th August 2016, 11:35 AM
 
 prices = {
-    "banana" : 4,
-    "apple"  : 2,
-    "orange" : 1.5,
-    "pear"   : 3,
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3,
 }
 stock = {
-    "banana" : 6,
-    "apple"  : 0,
-    "orange" : 32,
-    "pear"   : 15,
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15,
 }
 
 for key in prices:
