4aa81180b48d44ea9ccce86e795fc5de06b666a9
Amal Shehu <amalshehu@gmail.com>
2016-08-29 12:03:37 +0530

    values from dictionary

---
+++ supermarket.py
@@ -0,0 +1,23 @@
+#  File:       supermarket.py
+#  Purpose:    Example: Python dictionaty
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Monday 29th August 2016, 11:35 AM
+
+prices = {
+    "banana" : 4,
+    "apple"  : 2,
+    "orange" : 1.5,
+    "pear"   : 3,
+}
+stock = {
+    "banana" : 6,
+    "apple"  : 0,
+    "orange" : 32,
+    "pear"   : 15,
+}
+
+for key in prices:
+    print (key)
+    print ("price: %s" % prices[key])
+    print ("stock: %s" % stock[key])
