a385a8cb888aa333ffe70abbe93061f922f3107b
Amal Shehu <amalshehu@gmail.com>
2016-08-29 12:47:02 +0530

    Total added

--- supermarket.py
+++ supermarket.py
@@ -3,7 +3,7 @@
 #  Programmer: Amal Shehu
 #  Course:     Codecademy
 #  Date:       Monday 29th August 2016, 11:35 AM
-
+total = 0
 prices = {
     "banana": 4,
     "apple": 2,
@@ -21,3 +21,8 @@ for key in prices:
     print (key)
     print ("price: %s" % prices[key])
     print ("stock: %s" % stock[key])
+for key in prices:
+    total += prices[key]*stock[key]
+
+
+print total
