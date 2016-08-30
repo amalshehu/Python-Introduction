80606f56287ae716472359bc6350ee0f229d7d92
Amal Shehu <amalshehu@gmail.com>
2016-08-30 12:45:55 +0530

    Guess a number

---
+++ num_guess.py
@@ -0,0 +1,21 @@
+#  File:       lnum_guess.py
+#  Purpose:    Example : While loop test
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Tuesday 30th August 2016, 12:20 PM
+
+from random import randint
+
+# Generates a number from 1 through 10 inclusive
+random_number = randint(1, 10)
+
+guesses_left = 3
+# Start your game!
+while (guesses_left > 0):
+    guesses_left -= 1
+    guess = int(raw_input("Your guess: "))
+    if guess == random_number:
+        print ("You win!")
+        break
+else:
+    print ("You lose")
