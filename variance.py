33ad27c0364ef470a6d4d3a4794931b984f4fc3d
Amal Shehu <amalshehu@gmail.com>
2016-08-30 18:02:31 +0530

    [No issues ]convert indent to space / reformat files based on PEP8

--- variance.py
+++ variance.py
@@ -16,7 +16,7 @@ def grades_sum(grades):
     total = 0
     for grade in grades:
         total += grade
-    return (total)
+    return total
 
 
 def grades_average(grades):
@@ -32,33 +32,6 @@ def grades_variance(scores):
         variance = variance + (average - score) ** 2
     return variance / float(len(scores))
 
-print grades_variance(grades)
-
-grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
-
-def print_grades(grades):
-    for grade in grades:
-        print grade
-
-def grades_sum(grades):
-    total = 0
-    for grade in grades:
-        total += grade
-    return total
-
-def grades_average(grades):
-    sum_of_grades = grades_sum(grades)
-    average = sum_of_grades / float(len(grades))
-    return average
-
-def grades_variance(scores):
-    average = grades_average(scores)
-    variance = 0
-    for score in scores:
-        variance = variance + (average - score ) **2
-    return variance / float(len(scores))
-
-
 
 def grades_std_deviation(variance):
     return variance ** 0.5
@@ -66,8 +39,8 @@ def grades_std_deviation(variance):
 variance = grades_variance(grades)
 
 
-print print_grades(grades)
-print grades_sum(grades)
-print grades_average(grades)
-print variance
-print grades_std_deviation(variance)
+print (print_grades(grades))
+print (grades_sum(grades))
+print (grades_average(grades))
+print (variance)
+print (grades_std_deviation(variance))
