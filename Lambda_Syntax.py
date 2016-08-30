fd68f43d84032c58084684f0c6a4926fddea4b5d
Amal Shehu <amalshehu@gmail.com>
2016-08-30 21:02:49 +0530

    lambda:functional programming

---
+++ Lambda_Syntax.py
@@ -0,0 +1,8 @@
+#  File:       Lambda_Syntax.py
+#  Purpose:    Example : Functional Programming
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Tuesday 30th August 2016, 08:55 PM
+
+my_list = range(16)
+filter(lambda x: x % 3 == 0, my_list)
