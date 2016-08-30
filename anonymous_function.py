22b215f348b100c839d922804a2e083fd1c3ee94
Amal Shehu <amalshehu@gmail.com>
2016-08-30 21:00:16 +0530

    Initial commit

---
+++ anonymous_function.py
@@ -0,0 +1,5 @@
+#  File:       anonymous_funtion.py
+#  Purpose:    Example : Functional Programming
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Tuesday 30th August 2016, 08:50 PM
