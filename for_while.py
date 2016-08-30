b00091415837510b90b90ef02875a0a1922bf89a
Amal Shehu <amalshehu@gmail.com>
2016-08-30 12:01:33 +0530

    While loop test program

---
+++ for_while.py
@@ -0,0 +1,13 @@
+#  File:       for_while.py
+#  Purpose:    Example : While loop test
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Tuesday 30th August 2016, 11:56 AM
+
+count = 0
+
+if count < 5:
+    print ("Hello, I am an if statement and count is", count)
+while count < 5:
+    print ("Hello, I am a while and count is", count)
+    count += 1
