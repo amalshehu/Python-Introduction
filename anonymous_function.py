e132897dc736b95eaeb2ad20b9abc28ffc626d0b
Amal Shehu <amalshehu@gmail.com>
2016-08-30 22:10:43 +0530

    Secret message

--- anonymous_function.py
+++ anonymous_function.py
@@ -3,3 +3,7 @@
 #  Programmer: Amal Shehu
 #  Course:     Codecademy
 #  Date:       Tuesday 30th August 2016, 08:50 PM
+
+garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+message = filter(lambda x:  x!="X" ,garbled )
+print message
