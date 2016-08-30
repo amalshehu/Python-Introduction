ae356bfd758a9a4fe5dd765adc5c09b120a66230
Amal Shehu <amalshehu@gmail.com>
2016-08-30 23:21:31 +0530

    class created and printed values

--- person_class_.py
+++ person_class_.py
@@ -4,7 +4,7 @@
 #  Course:     Practice
 #  Date:       Tuesday 30th August 2016, 10:55 PM
 
-
+import datetime
 
 class Person:
 
@@ -16,3 +16,20 @@ class Person:
         self.address = address
         self.telephone = telephone
         self.email = email
+
+    def age(self):
+        today = datetime.date.today()
+        age = today.year - self.birthdate.year
+
+        if today < datetime.date(today.year, self.birthdate.month, self.birthdate.day):
+            age -= 1
+
+        return age
+
+person = Person("Amal","Shehu",datetime.date(1993, 7, 16),
+    "No. 12 Spike Street,Trivandrum","828 182 0382",
+    "amalshehu@gmail.com")
+
+print(person.name)
+print(person.email)
+print(person.age())
