b04f2a52971bed7d87a132451e2ae2bb44842d62
Amal Shehu <amalshehu@gmail.com>
2016-08-30 23:01:56 +0530

    Person info class created

---
+++ person_class_.py
@@ -0,0 +1,18 @@
+#  File:       person_class.py
+#  Purpose:    Example : Implementation of class (OOP)
+#  Programmer: Amal Shehu
+#  Course:     Practice
+#  Date:       Tuesday 30th August 2016, 10:55 PM
+
+
+
+class Person:
+
+    def __init__(self, name, surname, birthdate, address, telephone, email):
+        self.name = name
+        self.surname = surname
+        self.birthdate = birthdate
+
+        self.address = address
+        self.telephone = telephone
+        self.email = email
