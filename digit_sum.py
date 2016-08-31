db2a9b6726082e0d5d8ad477244289893e465073
Amal Shehu <amalshehu@gmail.com>
2016-08-31 18:28:58 +0530

    Error: list cant be passed

---
+++ digit_sum.py
@@ -0,0 +1,12 @@
+#  File:       digit_sum.py
+#  Purpose:    To find the sum of digits.
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Wednesday 31st August 2016, 06:20 PM
+
+x = int(input("Enter a number :"))
+list = [x]
+def digit_sum(list):
+    for item in list:
+        total += item
+    return total    
