67a21926d1d74e993612484940d117bd7d8e40e7
Amal Shehu <amalshehu@gmail.com>
2016-08-31 18:17:14 +0530

    Int or not

---
+++ is_int.py
@@ -0,0 +1,15 @@
+#  File:       is_even.py
+#  Purpose:    To check a number is an integer or not
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Wednesday 31st August 2016, 05:50 PM
+
+
+x = raw_input("Enter a number :")
+
+
+def is_int(x):
+    if x - int(x) > 0:
+        return True
+    else:
+        return False
