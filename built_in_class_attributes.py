cbf43cf6b7f3b486c407339e3d5edc2eae039dbc
Amal Shehu <amalshehu@gmail.com>
2016-09-05 23:11:47 +0530

    variables initialized &  member functions added

--- built_in_class_attributes.py
+++ built_in_class_attributes.py
@@ -3,3 +3,14 @@
 class Engineer:
    'Common base class for all Engineers'
    engCount = 0
+
+   def __init__(self, name, wage):
+      self.name = name
+      self.wage = wage
+      Engineer.engCount += 1
+
+   def displayCount(self):
+     print ("Total Engineer %d" % Engineer.engCount)
+
+   def displayEngineer(self):
+      print ("Name : ", self.name,  ", wage: ", self.wage)
