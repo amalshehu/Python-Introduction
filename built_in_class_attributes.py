ea121f6db2a5b7dd24a7d1cb42f304488e40bc76
Amal Shehu <amalshehu@gmail.com>
2016-09-05 23:21:05 +0530

    [No issues ]convert indent to space / reformat files based on PEP8

--- built_in_class_attributes.py
+++ built_in_class_attributes.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 
 # __dict__: Dictionary containing the class's namespace.
-#
+
 # __doc__: Class documentation string or none, if undefined.
-#
+
 # __name__: Class name.
-#
-# __module__: Module name in which the class is defined. This attribute is "__main__" in interactive mode.
-#
-# __bases__: A possibly empty tuple containing the base classes, in the order of their occurrence in the base class list.
-#
-#
+
+# __module__: Module name in which the class is defined. This attribute is
+# "__main__" in interactive mode.
+
+# __bases__: A possibly empty tuple containing the base classes, in the order
+#   of their occurrence in the base class list.
 
 
 class Engineer:
-   'Common base class for all Engineers'
-   engCount = 0
+    'Common base class for all Engineers'
+    engCount = 0
 
-   def __init__(self, name, wage):
-      self.name = name
-      self.wage = wage
-      Engineer.engCount += 1
+    def __init__(self, name, wage):
+        self.name = name
+        self.wage = wage
+        Engineer.engCount += 1
 
-   def displayCount(self):
-     print ("Total Engineer %d" % Engineer.engCount)
+    def displayCount(self):
+        print ("Total Engineer %d" % Engineer.engCount)
 
-   def displayEngineer(self):
-      print ("Name : ", self.name,  ", wage: ", self.wage)
+    def displayEngineer(self):
+        print ("Name : ", self.name,  ", wage: ", self.wage)
 
 emp1 = Engineer("Tom", 20000)
 emp2 = Engineer("Jerry", 50000)
@@ -34,4 +34,4 @@ print ("Engineer.__doc__:", Engineer.__doc__)
 print ("Engineer.__name__:", Engineer.__name__)
 print ("Engineer.__module__:", Engineer.__module__)
 print ("Engineer.__bases__:", Engineer.__bases__)
-print ("Engineer.__dict__:", Engineer.__dict__ )
+print ("Engineer.__dict__:", Engineer.__dict__)
