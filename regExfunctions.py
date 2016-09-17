440c1f5e0cef70c79db4ca8ee2727d8ffad714b4
Amal Shehu <amalshehu@gmail.com>
2016-09-17 23:12:40 +0530

    Add iterator yielding function for match objects

--- regExfunctions.py
+++ regExfunctions.py
@@ -5,3 +5,10 @@ string = "Many applications use Python as an embedded scripting language."
 # Return all words beginning with character 'a', as a list of strings
 print re.findall(r"\ba[\w]*", string)
 # ['applications', 'as', 'an']
+
+# Return all words beginning with character 'a', as an iterator yielding match objects
+_iter = re.finditer(r"\ba[\w]*", string)
+for match in _iter:
+    print "'{g}' was found between the indices {s}".format(g=match.group(), s=match.span())
+# 'as' was found between the indices (29, 31)
+# 'an' was found between the indices (32, 34)
